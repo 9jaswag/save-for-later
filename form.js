5c5006972b89f2b1b7b976ca281c0f71b04f72e1
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-02-18 21:26:19 +0100

    Add form saving functionality
    - add alert box with styling
    - add functionality for saving and populating form

--- form.js
+++ form.js
@@ -1,6 +1,50 @@
-// on page load
-// check if local storage has form DataCue, identified by url
-// check if the form exists
-// populate it
-// form.elements
-// for(item of form.elements)
+const formId = "save-later-form";
+const url = location.href;
+const formIdentifier = `${url} ${formId}`;
+
+const saveButton = document.querySelector("#save");
+const alertBox = document.querySelector(".alert");
+let form = document.querySelector(`#${formId}`);
+let formElements = form.elements;
+
+const getFormData = () => {
+  let data = { [formIdentifier]: {} };
+  for (const element of formElements) {
+    if (element.name.length > 0) {
+      data[formIdentifier][element.name] = element.value;
+    }
+  }
+  return data;
+};
+
+saveButton.onclick = event => {
+  event.preventDefault();
+  data = getFormData();
+  localStorage.setItem(formIdentifier, JSON.stringify(data[formIdentifier]));
+  const message = "Form draft has been saved!";
+  displayAlert(message);
+};
+
+const populateForm = () => {
+  if (localStorage.key(formIdentifier)) {
+    const savedData = JSON.parse(localStorage.getItem(formIdentifier));
+    for (const element of formElements) {
+      if (element.name in savedData) {
+        element.value = savedData[element.name];
+      }
+    }
+    const message = "Form has been refilled with saved data!";
+    displayAlert(message);
+    // localStorage.removeItem(formIdentifier);
+  }
+};
+
+const displayAlert = message => {
+  alertBox.innerText = message;
+  alertBox.style.display = "block";
+  setTimeout(function() {
+    alertBox.style.display = "none";
+  }, 1000);
+};
+
+document.onload = populateForm();
